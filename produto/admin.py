4b1f8fa541de06e38b3864446e6aa7378677b8de
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-12-26 18:38:20 -0300

    395mins estudados - 26/12/2023

---
+++ produto/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from produto.models import Produto, Variacao
+
+
+
+class VariacaoInLine(admin.TabularInline):
+    model = Variacao
+    extra = 1
+
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [
+        VariacaoInLine
+    ]
+
+
+
+admin.site.register(Produto, ProdutoAdmin)    
+admin.site.register(Variacao)
---
+++ produto/models.py
@@ -0,0 +1,94 @@
+from django.conf import settings
+from PIL import Image
+import os
+from django.db import models
+from pprint import pprint
+
+
+
+class Produto(models.Model):
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m', blank=True, null=True
+    )
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField(default=0)
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V', max_length=1, choices=(
+            ('V', 'Variável'), 
+            ('S', 'Simples')
+        ),
+    )
+
+
+    # Retorna o campo nome(dos produtos) quando este modelo é chamado, exemplo: QuerySet: <Produto: Celular> 
+    def __str__(self):
+        return self.nome
+    
+    
+    # Como o método estático não depende de uma instância específica, ele não tem acesso aos atributos específicos de uma instância. 
+    # Em vez disso, ele opera com base em seus próprios parâmetros ou lógica interna e é geralmente usado para funcionalidades que 
+    # estão associadas à classe como um todo, não a uma instância específica dela.
+    @staticmethod
+    def resize_imagem(original_img, new_width=800):
+        # Recebe a imagem e manda para o diretório MEDIA_ROOT definido em settings.py de loja_project
+        # img_full_path ---> É transformada em uma URL, por isso pegamos o: original_img.name do arquivo.
+        img_full_path = os.path.join(settings.MEDIA_ROOT, original_img.name)
+        img_pil = Image.open(img_full_path)  # Exemplo de retorno desta variável: (L:266, A:148)
+        original_width, original_height = img_pil.size  # Pega o tamanho da img (retorna uma tupla) atribui de acordo com os índices.
+        new_height = round((new_width * original_height) / original_width)  # Round --> Para retornar o inteiro mais próximo, precisa ser inteiro.
+
+        # Executa o bloco IF apenas se a condição for True...
+        if new_width >= original_width:
+            print()
+            print('Retornando, largura original MENOR ou IGUAL que nova largura!')
+            print()
+            img_pil.close()
+            return  # Execução do método resize_imagem será encerrada aqui mesmo. Sem continuar para as linhas subsequentes
+        
+        nova_imagem = img_pil.resize((new_width, new_height), Image.LANCZOS)  # Redimensiona a imagem com os novos parametros(New-w, New-h)
+        
+        # Salva a nova img por cima da original, passando o caminho que será salva, otimização e qualidade.
+        nova_imagem.save(img_full_path, optimize=True, quality=60)  
+        img_pil.close()
+        print()
+        print('Imagem foi redimensionada com sucesso!')
+        print()
+
+
+    def save(self, *args, **kwargs):
+        # Chama o super da classe pai para realmente salvar o produto cadastrado no DJANDO-ADMIN
+        super().save(*args, *kwargs)
+
+        max_image_size = 800  # Tamanho máximo da imagem
+
+        # Depois de chamar o método save do super (produto foi salvo), 
+        # Verifica se foi enviado também uma imagem, ou seja, se existe uma imagem naquele produto
+        # se sim, faz todo o bloco do if...
+        if self.imagem:
+            self.resize_imagem(self.imagem, max_image_size)
+
+
+
+class Variacao(models.Model):
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
+    
+
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)  # Deletar produto -> todas variações deste de produto será apagada também.
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return self.nome or self.produto.nome  # Nome da variação ou nome do produto que contém essa variação
